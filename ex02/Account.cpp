23df01512b02907a6d405a7ae75e58b3dc7bb177
evalldore <evalldore@gmail.com>
2023-11-18 22:21:55 -0500

    removed comments

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 17:32:30 by niceguy           #+#    #+#             */
-/*   Updated: 2023/11/18 22:20:21 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/11/18 22:21:47 by niceguy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,10 @@
 #include "Account.hpp"
 #define TIMESTAMP_FORMAT "[%04d%02d%02d_%02d%02d%02d]"
 
-/*
-
-static int	getNbAccounts( void );
-static int	getTotalAmount( void );
-static int	getNbDeposits( void );
-static int	getNbWithdrawals( void );
-static void	displayAccountsInfos( void );
-
-void	makeDeposit( int deposit );
-bool	makeWithdrawal( int withdrawal );
-int		checkAmount( void ) const;
-void	displayStatus( void ) const;
-
-*/
-
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
-
-//[19920104_091532] index:0;amount:42;created
 
 Account::Account(int initial_deposit)
 {
@@ -57,7 +40,7 @@ Account::~Account(void)
 	_displayTimestamp();
 	std::printf(" index:%d;amount:%d;closed\n", _accountIndex, _amount);
 }
-//[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+
 void	Account::makeDeposit( int deposit )
 {
 	int	last_amount;
@@ -70,19 +53,6 @@ void	Account::makeDeposit( int deposit )
 	_totalNbDeposits++;
 	_totalAmount += deposit;
 }
-
-/*
-
-[19920104_091532] index:0;p_amount:47;withdrawal:refused
-[19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
-[19920104_091532] index:2;p_amount:1521;withdrawal:657;amount:864;nb_withdrawals:1
-[19920104_091532] index:3;p_amount:434;withdrawal:4;amount:430;nb_withdrawals:1
-[19920104_091532] index:4;p_amount:1321;withdrawal:76;amount:1245;nb_withdrawals:1
-[19920104_091532] index:5;p_amount:23;withdrawal:refused
-[19920104_091532] index:6;p_amount:763;withdrawal:657;amount:106;nb_withdrawals:1
-[19920104_091532] index:7;p_amount:16596;withdrawal:7654;amount:8942;nb_withdrawals:1
-
-*/
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
@@ -136,8 +106,6 @@ int	Account::getNbWithdrawals( void )
 {
 	return (_totalNbWithdrawals);
 }
-
-//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
 
 void	Account::displayAccountsInfos( void )
 {
